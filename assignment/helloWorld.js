37cbbfb8302a15d78f85c14ce35ff4788581040b
AndyPrehn <133592836+AndyPrehn@users.noreply.github.com>
2023-08-19 20:00:28 -0500

    adding more style

---
+++ assignment/helloWorld.js
@@ -0,0 +1,14 @@
+console.log('hello world!');
+
+// variables
+// let is used to declare a variable
+// firstName is the name of our variable
+// 'Andy' is the value we assigned to our variable
+let firstName = 'Andy';
+console.log(firstName);
+
+let lastName = 'Prehn';
+console.log(lastName);
+
+let fullName = firstName + " " + lastName;
+console.log(fullName);
