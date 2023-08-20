2b2d7fc8cc2ee0a473c9ef08b5a532d7478846b2
AndyPrehn <133592836+AndyPrehn@users.noreply.github.com>
2023-08-20 11:10:08 -0500

    adding more Javascript

--- assignment/helloWorld.js
+++ assignment/helloWorld.js
@@ -16,3 +16,40 @@ console.log(fullName);
 //firstName, lastName, and fullName are 
 //all strings (text).
 
+// variable set to the number 80
+let temperature = 80;
+console.log(temperature);
+
+//boolean value type
+let loggedIn = true;
+//Console log with a string and value
+console.log('loggedIn', loggedIn);
+
+let isRaining = false;
+
+//Conditional statement
+// (should be either true or false)
+if(loggedIn) {
+    //if (true) will run this code
+    console.log('Welcome', fullName);
+} else {
+    console.log('Welcome, Gueat!');
+}
+
+// && which is and
+// || whic is or
+// true && false will be false
+// true && true will be true
+// true || false will be true
+
+// if the temperature is less than 85 
+//and it's not (!) raining. Let's go 
+//for a walk.
+if (temperature <85 && !isRaining){
+    console.log('Go for a walk!');
+} else if(isRaining) {
+    console.log('Bring an umbrella');
+} else {
+    console.log('Better to stay inside');
+}
+
